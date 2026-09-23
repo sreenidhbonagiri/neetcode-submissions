6f5f20f4121aa9c05cf84b64b7538036366d8464
Sreenidh Bonagiri <67754320+sreenidhbonagiri@users.noreply.github.com>
2026-09-23 18:20:08 -0400

    Add: two-integer-sum - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum/submission-15.py
@@ -0,0 +1,26 @@
+class Solution:
+    # We can make a index map that contains each number as the key and its value is just its index.
+    # Then we can loop through the nums list until we find the complement number to the target.
+    # Once we find it, then we can just return the index of the current number plus the complements index by returning the value. 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for index, number in enumerate(nums):
+            if number not in map:
+                map[number] = index
+
+        for index, number in enumerate(nums):
+            complement = target - number
+
+            if complement in map and map[complement] != index:
+                return sorted([map[complement], index])
+
+        
+                
+        
+
+     
+
+        
+                
+
+        
